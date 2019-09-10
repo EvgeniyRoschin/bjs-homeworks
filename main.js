d248aba1a9ede3d1c6e9e533afe154a93dd30009
Evgeniy Roschin <evgeny689@yandex.ru>
2019-09-10 23:35:00 +0400

    Задание к первой лекции

---
+++ main.js
@@ -0,0 +1,34 @@
+// Задание 1
+
+let algebra;
+let geography;
+let physics;
+
+algebra = 5;
+geography = 4;
+physics = 4;
+
+let avarage;
+
+avarage = (algebra + geography + physics) / 3;
+
+console.log(avarage);
+
+// Задание 2
+
+let name = 'Evgeniy';
+
+let message = `Привет, мир! Меня зовут ${name}.`;
+
+console.log(message);
+
+// Задание 3
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result;
+
+result = x * y + 5 * z + x - 1;
+
+console.log(result);
